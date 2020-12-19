89c59866ab8f3bf9722e45a2694ae4f10ac02149
boarchuz <46267286+boarchuz@users.noreply.github.com>
2020-12-20 03:29:18 +1100

    move headers to /src for Arduino

---
+++ src/hulp_arduino.h
@@ -0,0 +1,9 @@
+#include "hulp.h"
+
+#include "hulp_apa.h"
+#include "hulp_debug.h"
+#include "hulp_hall.h"
+#include "hulp_hx711.h"
+#include "hulp_i2cbb.h"
+#include "hulp_touch.h"
+#include "hulp_uart.h"
